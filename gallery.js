5a94d41ab9e7e2a22184c4f0bb42942d1178b795
zerdecz <37027555+zerdecz@users.noreply.github.com>
2018-07-29 14:46:57 +0200

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,9 +1,7 @@
 window.onload = function(){                 
   Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
   Galleria.run('.galleria');
-  var fs = require('fs');
-  var files = fs.readdirSync('/obrazky/');
-  alert(fs);
+
 
 }
 
